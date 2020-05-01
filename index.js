a471ac57afafc74e82d535cfce1363b5c0f5799d
giuliobosco <giuliobva@gmail.com>
2020-05-01 13:02:23 +0200

    add named constants, action creators

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 import { createStore } from "https://unpkg.com/redux@4.0.5/es/redux.mjs";
 // import { createStore } from 'redux';
 
+// NAMED CONSTANTS
+
+const BUTTON_CLICKED = 'BUTTON_CLICKED';
+const MODAL_CLOSED = 'MODAL_CLOSED';
+
+// ACTION CREATORS
+function buttonClicked(payload) {
+    return {
+        type: BUTTON_CLICKED,
+        payload,
+    }
+}
+
+function modalClosed() {
+    return { type: MODAL_CLOSED };
+}
+
 const initialState = {
     buttonClicked: 'no',
     modalClosed: 'no',
 };
 
 function rootReducer(state=initialState, action) {
-   /* if (action.type === 'BUTTON_CLICKED') {
+   /* if (action.type === BUTTON_CLICKED) {
         //change the state
     }
-    if (action.type === 'MODAL_CLOSED') {
+    if (action.type === MODAL_CLOSED) {
        // change state for modal closed 
     }
     return state;
     */
     switch (action.type) {
-        case 'BUTTON_CLICKED':
+        case BUTTON_CLICKED:
             state.buttonClicked = "yes"; // TODO fix me, no correct redux use
             return state;
-        case 'MODAL_CLOSED':
+        case MODAL_CLOSED:
             state.modalClosed = "yes"; // TODO fix me, no correct redux use
             return state;
 
@@ -32,10 +49,10 @@ const store = createStore(rootReducer);
 
 // UI
 const button = document.getElementsByTagName('button')[0];
-button.addEventListener('click', () => store.dispatch({ type: 'BUTTON_CLICKED' }));
+button.addEventListener('click', (event) => store.dispatch(buttonClicked(event)));
 
 const buttonModal = document.getElementById('buttonModal');
-buttonModal.addEventListener('click', () => store.dispatch({ type: 'MODAL_CLOSED' }));
+buttonModal.addEventListener('click', () => store.dispatch(modalClosed()));
 
 store.subscribe(() => {
     if (store.getState().buttonClicked === 'yes') {
